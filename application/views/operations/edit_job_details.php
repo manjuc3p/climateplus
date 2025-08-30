18d848f40fda4c9acd3f869e987016657de172fc
manjuc3p <manjuanand.exp.bookmarks@gmail.com>
2025-08-30 12:42:54 +0530

    last

--- application/views/operations/edit_job_details.php
+++ application/views/operations/edit_job_details.php
@@ -73,7 +73,7 @@
 			<div class="col-xs-12 col-sm-9 col-md-4 col-lg-4">
 				<textarea name="job_desc" id="job_desc" rows='5' cols='40' class="form-control form-control-sm" tabindex="4" required><?php echo $job['job_desc'] ? $job['job_desc'] : $job['job_title'];  ?></textarea>
 			</div>
-			<label class="col-xs-12 col-sm-2 col-md-2 col-lg-2 col-form-label">Additional Notes:</label>
+			<label class="col-xs-12 col-sm-2 col-md-2 col-lg-2 col-form-label">Additional Notesh:</label>
 			<div class="col-xs-12 col-sm-9 col-md-4 col-lg-4">
 				<textarea name="addnotes" id="addnotes" class="form-control form-control-sm" rows="5"><?php echo $job['additional_notes'] ?? ''; ?></textarea>
 			</div>
